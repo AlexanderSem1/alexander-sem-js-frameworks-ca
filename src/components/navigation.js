403899b3b037ea42be2a0e14adf0ae6b8444bb7d
Alexander Sem <alexander.sem@icloud.com>
2020-04-25 18:35:45 +0200

    font height fix

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
 
     return (
-        <nav className="[ navbar ]">
-            <ul className="[ navbar-nav ]">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+        <nav className="[ navbar navbar-dark ]">
+            <ul className="[ nav navbar-nav  ]">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
             </ul>
         </nav>
     )
